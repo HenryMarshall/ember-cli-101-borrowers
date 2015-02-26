a81d567d9d31c5e19addd6a59738b162bf608e30
hbaughman <hbaughman741@gmail.com>
2015-02-26 13:43:06 -0500

    Refactor new/edit actions into base

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,47 +1,11 @@
-import Ember from 'ember';
+// Here we extend the functionality of a shared (with edit) base controller.
+// `FriendsBaseController` is just a variable name for the default export value.
+import FriendsBaseController from './base';
 
-export default Ember.Controller.extend({
-  // Ember.Computed allows us to create function that are treated as properties.
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    function() {
-      return !Ember.isEmpty(this.get('model.email')) &&
-             !Ember.isEmpty(this.get('model.firstName')) &&
-             !Ember.isEmpty(this.get('model.lastName')) &&
-             !Ember.isEmpty(this.get('model.twitter'));
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-    save: function() {
-      if (this.get('isValid')) {
-        var _this = this;
-        // `.save()` will return a promise. When it is fulfilled, the anonymous
-        // `.function gets the `friend` as it's arg.
-        this.get('model').save().then(function(friend) {
-          // We then transition to this (edited) `friend`.
-          _this.transitionToRoute('friends.show', friend);
-        });
-      }
-      else {
-        this.set('errorMessage', 'You have to fill out all the fields');
-      }
-
-      return false;
-
-      // // To watch the event bubble, you must return true.
-      // console.log("+- save action in friends new controller");
-      // // You can make the event bubble by returning true. We will handle this
-      // // up through the various routes (FriendsNewController > 
-      // // FriendsNewRoute > FriendsRoute > ApplicationRoute).
-      // return true;
-    },
+    // Here we override the cancel action, with new specific behavior.
     cancel: function() {
-
-      // `transitionToRoute()` functions similarly to {{link-to}}, but within a
-      // controller.
       this.transitionToRoute('friends');
       return false;
 
