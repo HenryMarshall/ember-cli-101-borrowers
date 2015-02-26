380881e07a7fbfc8a0b3c2adfe3e135266a02d92
hbaughman <hbaughman741@gmail.com>
2015-02-26 12:59:59 -0500

    Add explicit model for illustation

--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  // This is an explicit writing of the default configuration. Ember knows to
+  // generate this because the :dynamic_segment in app/router.js ends in `_id`.
+
+  model: function(params) {
+    return this.store.find('friend', params.friend_id);
+  }
+
 });
