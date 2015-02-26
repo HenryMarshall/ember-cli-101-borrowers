839022773df0af91e31efa134ef140c2e7aeb0e6
hbaughman <hbaughman741@gmail.com>
2015-02-26 10:00:21 -0500

    Validate presence of FriendNew params

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -11,24 +11,43 @@ export default Ember.Controller.extend({
       return !Ember.isEmpty(this.get('model.email')) &&
              !Ember.isEmpty(this.get('model.firstName')) &&
              !Ember.isEmpty(this.get('model.lastName')) &&
-             !Ember.isEmpty(this.get('model.twitter'))
+             !Ember.isEmpty(this.get('model.twitter'));
     }
   ),
   actions: {
     save: function() {
-      console.log("+- save action in friends new controller");
+      if (this.get('isValid')) {
+        var _this = this;
+        // `.save()` will return a promise. When it is fulfilled, the anonymous
+        // `.function gets the `friend` as it's arg.
+        this.get('model').save().then(function(friend) {
+          // We then transition to this (edited) `friend`.
+          _this.transitionToRoute('friends.show', friend);
+        });
+      }
+      else {
+        this.set('errorMessage', 'You have to fill out all the fields');
+      }
 
-      // You can make the event bubble by returning true. We will handle this up
-      // through the various routes (FriendsNewController > FriendsNewRoute >
-      // FriendsRoute > ApplicationRoute).
-      return true;
+      return false;
+
+      // // To watch the event bubble, you must return true.
+      // console.log("+- save action in friends new controller");
+      // // You can make the event bubble by returning true. We will handle this
+      // // up through the various routes (FriendsNewController > 
+      // // FriendsNewRoute > FriendsRoute > ApplicationRoute).
+      // return true;
     },
     cancel: function() {
-      console.log("+- cancel action in friends new controller");
 
-      // To easily see where an event will bubble, check `Current Route Only` in
-      // the Ember Inspector.
-      return true;
+      this.transitionToRoute('friends');
+      return false;
+
+      // // To watch the event bubble you must return true.
+      // console.log("+- cancel action in friends new controller");
+      // // To easily see where an event will bubble, check `Current Route Only` in
+      // // the Ember Inspector.
+      // return true;
     }
   }
 });
