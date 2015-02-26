28e5af9f4758a14c113f4c07710669a6583cc342
hbaughman <hbaughman741@gmail.com>
2015-02-26 08:39:33 -0500

    Set friends/index route

---
+++ app/routes/friends/index.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    // You can search for a record by id thusly:
+    //      return this.store.find('friend', 782);
+    // which requests `/api/friends/1`. This will return a single object, so 
+    // corresponding modifications must be made to the template.
+
+    // You can query for a subset thusly:
+    //      return this.store.findQuery('friend', { active: true });
+    // which requests `/api/friends?active=true`
+
+    // I am unsure about the difference between the executed command and:
+    //      return this.store.find('friend');
+
+    return this.store.findAll('friend');
+
+    // Weirdly, I am now struggling to run the above manually with `$E`. I have
+    // tried the ApplicationRoute, FriendsRoute and FriendsIndexRoute.
+  }
+});
