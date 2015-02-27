3ef8050b601eb77421a677fe4cc02d00fd73480a
hbaughman <hbaughman741@gmail.com>
2015-02-27 13:14:49 -0500

    Renamed example adapter causing interference

--- app/adapters/foo.js
+++ app/adapters/foo.js
@@ -2,9 +2,9 @@
 /*global ApplicationAdapter:false*/
 
 export default ApplicationAdapter.extend({
-  // If the server uses nested urls, you might need to customize the adderss for
+  // If the server uses nested urls, you might need to customize the address for
   // a single resource.
   buildURL: function(type, id, record) {
-    return '/friends/' + record.get('friend.id') + '/articles/' + id;
+    return '/friends/' + record.get('friend.id') + '/foo/' + id;
   } 
 });
