00f56972b9ceda909b02125325f85e9f2f38bc33
hbaughman <hbaughman741@gmail.com>
2015-02-27 21:39:01 -0500

    Set article-row component characteristics

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // This component will always be a 'tr', default is 'div'.
+  tagName: 'tr',
+  article: null,          // passed-in
+  articleStates: null,    // passed-in
+  actions: {
+    saveArticle: function(article) {
+      // Actions in components don't automatically bubble. So to trigger an
+      // action in a Route or Controller, we'll need to bind suck action to a
+      // property and then call it with `sendAction`.
+      this.sendAction('save', article);
+    }
+  }
 });
