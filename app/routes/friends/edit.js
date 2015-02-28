13a3da41674d49894ce226511c3bac0a72d5ea66
hbaughman <hbaughman741@gmail.com>
2015-02-27 22:11:34 -0500

    Rollback changes to canceled edits

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  deactivate: function() {
+    var model = this.modelFor('friends/edit');
+    // This restores a dirty model to their initial state.
+    model.rollback();
+  }
 });
