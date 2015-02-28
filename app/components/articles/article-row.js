d1fc6d6794edd51685d5d4e12e70dd2a2bc4f5dd
hbaughman <hbaughman741@gmail.com>
2015-02-27 22:01:07 -0500

    Implement auto-save

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -5,12 +5,29 @@ export default Ember.Component.extend({
   tagName: 'tr',
   article: null,          // passed-in
   articleStates: null,    // passed-in
-  actions: {
-    saveArticle: function(article) {
-      // Actions in components don't automatically bubble. So to trigger an
-      // action in a Route or Controller, we'll need to bind suck action to a
-      // property and then call it with `sendAction`.
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
       this.sendAction('save', article);
     }
-  }
+  },
+  stateChanged: function() {
+    var article = this.get('article');
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  // The `observes` method watches for changes. The check is also run on
+  // initialization.
+  }.on('init').observes('article.state')
+
+  // // The `saveArticle` action was triggered by the (deprecated) save button
+  // // found in `app/templates/components/articles/articles-row.hbs`.
+  // actions: {
+  //   saveArticle: function(article) {
+  //     // Actions in components don't automatically bubble. So to trigger an
+  //     // action in a Route or Controller, we'll need to bind suck action to a
+  //     // property and then call it with `sendAction`.
+  //     this.sendAction('save', article);
+  //   }
+  // }
 });
